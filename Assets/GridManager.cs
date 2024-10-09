1a8887216b74f3b8150313d12c96b66b680d41f1
MircaSalvatorul <mateimircea919@gmail.com>
2024-10-09 22:21:38 +0300

    Board Edge
    Now it creates the tiles only on the edge of the "board"

--- Assets/GridManager.cs
+++ Assets/GridManager.cs
@@ -16,17 +16,21 @@ public class GridManager : MonoBehaviour
         for (int x = 0; x < width; x++)
         {
             for (int y = 0; y < height; y++)
-            {
-                // Create a new cube
-                Tile tile = Instantiate(tilePrefab, new Vector3(x, y, 0), Quaternion.identity); 
-                tile.name = "Tile " + x + ", " + y;
+            {   //To create only the edge of the grid
+                if (x == 0 || y == 0 || y == height - 1 || x == width - 1)
+                {
+                    // Create a new cube
+                    Tile tile = Instantiate(tilePrefab, new Vector3(x, y, 0), Quaternion.identity);
+                    tile.name = "Tile " + x + ", " + y;
 
-                var isOffset = (x+y) % 2 == 1; // Check if the tile is offset
-                tile.Init(isOffset); // Set the color of the tile
+                    var isOffset = (x + y) % 2 == 1; // Check if the tile is offset
+                    tile.Init(isOffset); // Set the color of the tile
+                }
             }
         }
 
-        cam.transform.position = new Vector3((width / 2f) -0.5f, (height / 2f) - 0.5f, -10); // Move the camera to the center of the grid
+
+        cam.transform.position = new Vector3((width / 2f) - 0.5f, (height / 2f) - 0.5f, -10); // Move the camera to the center of the grid
     }
 
     // Start is called before the first frame update
