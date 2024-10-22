4c18acfde617a62d1e74b14a98fea7973b0edeb4
MircaSalvatorul <mateimircea919@gmail.com>
2024-10-22 19:11:29 +0300

    Questions and Answer scripts
    [In progress stuff]

---
+++ Assets/QuestionAndAnwers.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+
+public class Question
+{
+    public string Intrebare;
+    public string A;
+    public string B;
+    public string C;
+    public string D;
+    public string RaspunsCorect;
+}
+
+public class QuestionAndAnwers: MonoBehaviour
+{
+    public string question;
+    public string[] answers;
+    public int correctAnswer;
+
+    public List<Question> questions = new List<Question>();
+
+    void Start()
+    {
+        string filePath = Path.Combine(Application.streamingAssetsPath, "intrebari.csv");
+        ReadCSV(filePath);
+    }
+
+    void ReadCSV(string filePath)
+    {
+        StreamReader reader = new StreamReader(filePath);
+        bool firstLine = true;
+
+        while (!reader.EndOfStream)
+        {
+            string line = reader.ReadLine();
+            if (firstLine) // Skip the first line (headers)
+            {
+                firstLine = false;
+                continue;
+            }
+
+            string[] values = line.Split(',');
+            Question newQuestion = new Question
+            {
+                Intrebare = values[0],
+                A = values[1],
+                B = values[2],
+                C = values[3],
+                D = values[4],
+                RaspunsCorect = values[5]
+            };
+            questions.Add(newQuestion);
+        }
+        reader.Close();
+    }
+}
---
+++ Assets/QuizManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuizManager : MonoBehaviour
+{
+    public QuestionAndAnswers QnA;
+    public GameObject[] options;
+    public int currentQuestion;
+
+    public Text QuestionText;
+
+    public void Start()
+    {
+
+    }
+
+    void GenerateQuestions()
+    {
+
+    }
+}
