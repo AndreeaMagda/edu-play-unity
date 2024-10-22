1ca4f36ab7d182811ce66b2eb45bf3bff4fffb3d
MircaSalvatorul <mateimircea919@gmail.com>
2024-10-23 01:55:15 +0300

    Finished Quiz-type dialogue

--- Assets/Answers.cs
+++ Assets/Answers.cs
@@ -4,5 +4,19 @@ using UnityEngine;
 
 public class Answers : MonoBehaviour
 {
+    public bool isCorrect = false;
+    public QuizManager quizManager;
 
+    public void Answer()
+    {
+        if (isCorrect)
+        {
+            Debug.Log("Correct Answer");
+            quizManager.Correct();
+        }
+        else
+        {
+            Debug.Log("Wrong Answer");
+        }
+    }
 }
---
+++ Assets/QuizManager.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class QuizManager : MonoBehaviour
+{
+    public QuestionAndAnswers QnA;
+    public GameObject[] options;
+    public int currentQuestion;
+
+    public TMP_Text QuestionText;
+
+    public void Start()
+    {
+        GenerateQuestions();
+    }
+
+    public void Correct()
+    {
+
+        QnA.questions.RemoveAt(currentQuestion);
+        GenerateQuestions();
+    }
+
+    void SetAnswers()
+    {
+        for (int i = 0; i< options.Length; i++)
+        {
+            options[i].GetComponent<Answers>().isCorrect = false; // Corrected from Answer to Answers
+
+            options[i].transform.GetChild(0).GetComponent<Text>().text = QnA.questions[currentQuestion].answers[i];
+
+            if (QnA.questions[currentQuestion].correctAnswer == i)
+            {
+                options[i].GetComponent<Answers>().isCorrect = true;
+            }
+
+        }
+
+    }
+
+    void GenerateQuestions()
+    {
+        currentQuestion = Random.Range(0, QnA.questions.Count);
+
+        QuestionText.text = QnA.questions[currentQuestion].Intrebare;
+        SetAnswers();
+
+    }
+}
